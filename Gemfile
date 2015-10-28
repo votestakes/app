218b8a68f7ac75ea9cbe66853bbbee33fcc1d843
Ross-Hunter <ross-hunter@ross-hunter.com>
2015-10-27 23:26:22 -0400

    Add Facebook login

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '~>4.2'
 gem 'pg'
 gem 'dalli'
 gem 'rails_12factor'
+gem 'dotenv-rails'
 
 # ActiveRecord
 gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
+  devise_for :users, controllers: {
+                        omniauth_callbacks: 'omniauth_callbacks' }
   root 'challenges#index'
 
+
+  resources :users
   resources :challenges
 
 end

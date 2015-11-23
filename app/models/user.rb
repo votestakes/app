cae229b23e9e436c11d095706fbf9b38355e6049
Ross-Hunter <ross-hunter@ross-hunter.com>
2015-11-23 18:00:42 -0500

    Move Facebook initializer back into devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,12 +17,12 @@ class User < ActiveRecord::Base
 
   class << self
     def from_omniauth(auth)
-     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-       user.email = auth.info.email
-       user.password = Devise.friendly_token[0,20]
-       user.name = auth.info.name   # assuming the user model has a name
-       # user.image = auth.info.image # assuming the user model has an image
-     end
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.name = auth.info.name   # assuming the user model has a name
+        # user.image = auth.info.image # assuming the user model has an image
+      end
     end
 
     def new_with_session(params, session)
